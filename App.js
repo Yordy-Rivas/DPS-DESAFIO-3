823200d8133a117b7407b9488e9b766add1652f9
XxxDavoxxXgit config --global user.email davidfranco2802@gmail.com <davidfranco2802@gmail.com>
2026-05-22 21:18:55 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,60 +1,6 @@
-import AppNavigator from "./src/navigation/AppNavigator";
-import React, { useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-
-import { onAuthStateChanged } from 'firebase/auth';
-
-import { auth } from './src/services/firebaseConfig';
-
+import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
-import LoginScreen from './src/screens/LoginScreen';
 
-import {
-  View,
-  ActivityIndicator
-} from 'react-native';
-
-export default function App() {
-
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-
-      setUser(currentUser);
-      setLoading(false);
-
-    });
-
-    return unsubscribe;
-
-  }, []);
-
-  if (loading) {
-
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <ActivityIndicator size="large" />
-      </View>
-    );
-
-  }
-
-  return (
-    <NavigationContainer>
-      {user ? <AppNavigator /> : <LoginScreen />}
-    </NavigationContainer>
-  );
-
-}
 export default function App() {
   return <AppNavigator />;
 }
