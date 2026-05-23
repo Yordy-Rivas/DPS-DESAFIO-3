7e5f9395e08c47dd684325577469d073d469281c
XxxDavoxxXgit config --global user.email davidfranco2802@gmail.com <davidfranco2802@gmail.com>
2026-05-22 21:48:13 -0600

    Update App.js
    App. js modificado

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
+import AppNavigator
+from "./src/navigation/AppNavigator";
 
 export default function App() {
+
   return <AppNavigator />;
+
 }
